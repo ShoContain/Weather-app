89ceffe6d14548d0e6f6a583652c95af8ff5d60c
ShoContain <perblue.666@gmail.com>
2020-05-01 15:07:57 +0900

    天気予報アプリ

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="/css/main.css">
+    <script src="/js/app.js" defer></script>
+    <title>Weather App</title>
+</head>
+<body class="bg-blue-500">
+    <div id="app">
+        <weather-app></weather-app>
+    </div>
+</body>
+</html>
